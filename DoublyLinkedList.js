6a855b419d4a677798eebabfe5dec1ddd538ff81
Johannes Baum <baum@bw-webgestaltung.de>
2019-05-19 11:11:08 +0200

    initial commit

---
+++ DoublyLinkedList.js
@@ -0,0 +1,65 @@
+const Node = data => ({ data: data, next: null, prev: null });
+
+module.exports = () => {
+  let head = null;
+  let size = 0;
+
+  const invertIndex = (index) => size - index - 1;
+
+  const getNode = index => {
+    const invertedIndex = invertIndex(index);
+    if (invertedIndex === 0) return head;
+
+    let currentIndex = 0;
+    let currentNode = head;
+    while (currentIndex < invertedIndex && currentNode != null) {
+      currentIndex++;
+      currentNode = currentNode.next;
+    }
+    return currentNode;
+  };
+
+  const getNodeData = index => {
+    const node = getNode(index);
+    if (node === null) return null;
+    return node.data;
+  }
+
+  const getSize = () => size;
+
+  const add = data => {
+    if (head == null) {
+      head = Node(data);
+      size = 1;
+    } else {
+      const oldHead = head;
+      head = Node(data);
+      head.next = oldHead;
+      size++;
+    }
+  };
+
+  const remove = index => {
+    const invertedIndex = invertIndex(index);
+    if (invertedIndex === 0) {
+      const oldHead = head;
+      head = head.next;
+      size--;
+      return oldHead;
+    }
+
+    const prev = getNode(index + 1);
+    if (prev === null) return null;
+    size--;
+    const deletedNode = prev.next;
+    prev.next = deletedNode.next;
+    return deletedNode;
+  };
+
+  return {
+    size: getSize,
+    add,
+    get: getNodeData,
+    remove,
+  };
+};
---
+++ README.md
@@ -0,0 +1,38 @@
+# parrot-sll
+
+A doubly linked list in JavaScript.
+
+## Usage
+
+```
+const ParrotDll = require("parrot-dll");
+
+const addressList = ParrotDll();
+addressList.add({ name: 'Bruce Lee', street: 'Kung Fu Street 12' });
+addressList.add({ name: 'John Snow', street: 'Winterfell 1' });
+
+console.log(addressList.size()) // "2"
+
+console.log(addressList.get(0).name) // "Bruce Lee"
+console.log(addressList.get(1).name) // "John Snow"
+
+addressList.remove(0);
+
+console.log(addressList.size()) // "1"
+
+console.log(addressList.get(0).name) // "John Snow"
+```
+
+## Methods
+
+- `add(object: any): void` Adds an object to the list
+- `size(): number` Returns the size of the list
+- `get(index: integer): any` Returns object at given index. If index is out of bounds it returns null.
+- `remove(index: integer): any` Removes the object at given index from the list and returns the removed object. If the index is out of bounds it returns null.
+
+## Runtime complexity
+
+- `add`: O(1)
+- `size`: O(1)
+- `get`: O(n)
+- `remove`: O(n)
